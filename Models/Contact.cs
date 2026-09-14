0e1a4d9160fe33b2c939c04195b27d63f5de5011
vanshika <vanshika1407@gmail.com>
2026-09-14 09:47:02 +0530

    UC1: create Contact class with ToString

---
+++ Models/Contact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp.Models
+{
+    
+        public class Contact
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+
+            public Contact(
+                string firstName,
+                string lastName,
+                string address,
+                string city,
+                string state,
+                string zip,
+                string phoneNumber,
+                string email)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Address = address;
+                City = city;
+                State = state;
+                Zip = zip;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+
+            public override string ToString()
+            {
+                return $"{FirstName} {LastName} | {Address}, " + $"{City}, {State} {Zip} | {PhoneNumber} | {Email}";
+            }
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using AddressBookApp.Models;
+
+namespace AddressBookApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact(
+                "Vanshika",
+                "Chhabra",
+                "12 MG Road",
+                "Ambala",
+                "Haryana",
+                "134003",
+                "9876543210",
+                "vanshika@gmail.com"
+            );
+
+            Console.WriteLine(contact.ToString());
+        }
+    }
+}
